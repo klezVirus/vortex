f037cc05968968ccea0c86080d784d02bbf1395b
d3adc0de <klez.virus@gmail.com>
2023-03-05 22:46:46 +0000

    Switching from workers to thread pools

--- enumerators/factories.py
+++ enumerators/factories.py
@@ -1,16 +1,35 @@
-import traceback
 from pydoc import locate
+from typing import Union
 
-from enumerators.enumerator import VpnEnumerator
+from enumerators.interfaces.enumerator import VpnEnumerator
+from enumerators.interfaces.searcher import Searcher
 
 
 class VpnEnumeratorFactory:
     @staticmethod
-    def from_name(name: str, target, group=None) -> VpnEnumerator:
+    def from_name(name: str, target, group=None) -> Union[VpnEnumerator, None]:
         enumerator_class = None
         try:
             enumerator_class_string = f"enumerators.vpn.{name.lower()}.{name.capitalize()}Enumerator"
             # print(enumerator_class_string)
+            enumerator_class = locate(enumerator_class_string)
+            return enumerator_class(target, group=group)
+        except TypeError as e:
+            if str(e).find("NoneType") != -1:
+                return None
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            print(e.__class__.__name__ + ": " + e.__str__())
+            return None
+
+
+class OfficeEnumeratorFactory:
+    @staticmethod
+    def from_name(name: str, target, group=None) -> Union[VpnEnumerator, None]:
+        enumerator_class = None
+        try:
+            enumerator_class_string = f"enumerators.office.{name.lower()}.{name.capitalize()}Enumerator"
             enumerator_class = locate(enumerator_class_string)
             return enumerator_class(target, group=group)
         except Exception as e:
@@ -19,15 +38,30 @@ class VpnEnumeratorFactory:
             return None
 
 
-class OfficeEnumeratorFactory:
+class MiscEnumeratorFactory:
     @staticmethod
-    def from_name(name: str, target, group=None) -> VpnEnumerator:
+    def from_name(name: str, target, group=None) -> Union[VpnEnumerator, None]:
         enumerator_class = None
         try:
-            enumerator_class_string = f"enumerators.office.{name.lower()}.{name.capitalize()}Enumerator"
+            enumerator_class_string = f"enumerators.misc.{name.lower()}.{name.capitalize()}Enumerator"
             enumerator_class = locate(enumerator_class_string)
             return enumerator_class(target, group=group)
         except Exception as e:
             # traceback.print_exc()
+            # print(e.__class__.__name__ + ": " + e.__str__())
+            return None
+
+
+class SearcherFactory:
+    @staticmethod
+    def from_name(name: str) -> Union[Searcher, None]:
+        enumerator_class = None
+        try:
+            enumerator_class_string = f"enumerators.search.{name.lower()}.{name.capitalize()}"
+            enumerator_class = locate(enumerator_class_string)
+            return enumerator_class()
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
             # print(e.__class__.__name__ + ": " + e.__str__())
             return None
--- enumerators/parallel.py
+++ enumerators/parallel.py
@@ -17,21 +17,23 @@ class Worker(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.threading_object = None
+        self.lock = threading.Lock()
 
     def run(self):
         while True:
-            username, password = self.threading_object.dequeue()
+            username, password, group = self.threading_object.dequeue()
             if not username or not password:
                 return
             try:
-                result, res = self.threading_object.safe_login(username, password)
+                result, res = self.threading_object.safe_login(username, password, group=group)
                 msg = f"RESULT: {'SUCCESS' if result else 'FAILED'}; RESPONSE: {pickle.dumps(res) if res else ''}"
                 self.threading_object.logger.debug(msg)
                 if result:
                     self.threading_object.add_valid_login(username, password)
-                    success(f"{username:50}:{password:50} is valid! -- CODE: {res.status_code} ; LEN: {len(res.text):8}", indent=2)
+                    success(f"{username:50}:{password:50}:{self.threading_object.group:20} is valid! -- CODE: {res.status_code} ; LEN: {len(res.text):8}", lock=self.lock)
                 else:
-                    error(f"{username:50}:{password:50} is not valid. -- CODE: {res.status_code} ; LEN: {len(res.text):8}", indent=2)
+                    with threading.Lock():
+                        error(f"{username:50}:{password:50}:{self.threading_object.group:20} is not valid. -- CODE: {res.status_code} ; LEN: {len(res.text):8}", lock=self.lock)
             except KeyboardInterrupt:
                 error("Aborted by user")
                 multiprocessing.current_process().terminate()
@@ -52,85 +54,22 @@ class DetectWorker(threading.Thread):
             if enumerator is None:
                 continue
             try:
-                endpoint_type_name = EndpointType.get_name(endpoint_type_label)
+                endpoint_type_name = self.threading_object.dbms.get_etype_name(endpoint_type_label)
                 result, res = enumerator.safe_validate()
                 msg = f"RESULT: {'SUCCESS' if result else 'FAILED'}; RESPONSE: {pickle.dumps(res) if res else ''}"
+
                 if result:
                     self.threading_object.add_endpoint(enumerator, endpoint_type_label)
                     if self.threading_object.debug:
-                        success(f"{enumerator.target.strip()} is a {endpoint_type_name} endpoint", indent=2)
+                        success(f"{enumerator.target.strip()} is a {endpoint_type_name} endpoint")
                 else:
-                    self.threading_object.add_endpoint(enumerator, EndpointType.UNKNOWN.value)
                     if self.threading_object.debug:
-                        error(f"{enumerator.target.strip()} is not a {endpoint_type_name} endpoint", indent=2)
+                        error(f"{enumerator.target.strip()} is not a {endpoint_type_name} endpoint")
             except KeyboardInterrupt:
                 error("Aborted by user")
                 multiprocessing.current_process().terminate()
             except Exception as e:
-                # traceback.print_exc()
+                traceback.print_exc()
                 debug(f"Exception: {enumerator.__class__.__name__} - {e}")
             self.threading_object.done()
 
-
-class UserEnumWorker(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.threading_object = None
-
-    def run(self):
-        # Worker to use when an endpoint allows for user enumeration
-        # TODO: The code below is WRONG. Fix it
-
-        while True:
-            enumerator, endpoint_type_label = self.threading_object.dequeue()
-            if enumerator is None:
-                continue
-            try:
-                endpoint_type_name = EndpointType.get_name(endpoint_type_label)
-                result, res = enumerator.safe_validate()
-                msg = f"RESULT: {'SUCCESS' if result else 'FAILED'}; RESPONSE: {pickle.dumps(res) if res else ''}"
-                if result:
-                    self.threading_object.add_endpoint(enumerator.target, endpoint_type_label)
-                    if self.threading_object.debug:
-                        success(f"{enumerator.target.strip()} is a {endpoint_type_name} endpoint", indent=2)
-                else:
-                    if self.threading_object.debug:
-                        error(f"{enumerator.target.strip()} is not a {endpoint_type_name} endpoint", indent=2)
-            except Exception as e:
-                traceback.print_exc()
-                debug(f"Exception: {e}")
-            self.threading_object.done()
-
-
-class ValidateWorker(threading.Thread):
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.threading_object = None
-
-    def run(self):
-        # To refactor, we would like parallel validation for users and endpoints
-        # TODO: The code below is WRONG. Fix it
-
-        while True:
-            session, url = self.threading_object.dequeue()
-            enumerator, endpoint_type_label = self.threading_object.dequeue()
-
-            if enumerator is None:
-                continue
-            try:
-                endpoint_type_name = EndpointType.get_name(endpoint_type_label)
-                result, res = enumerator.safe_validate()
-                msg = f"RESULT: {'SUCCESS' if result else 'FAILED'}; RESPONSE: {pickle.dumps(res) if res else ''}"
-                if result:
-                    self.threading_object.add_endpoint(enumerator.target, endpoint_type_label)
-                    if self.threading_object.debug:
-                        success(f"{enumerator.target.strip()} is a {endpoint_type_name} endpoint", indent=2)
-                else:
-                    if self.threading_object.debug:
-                        error(f"{enumerator.target.strip()} is not a {endpoint_type_name} endpoint", indent=2)
-            except Exception as e:
-                traceback.print_exc()
-                debug(f"Exception: {e}")
-            self.threading_object.done()
